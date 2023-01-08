5ee5e574747ccc0cbed4a43b9b705f1c0d59a3cd
annaSchugay <anya.schugay@gmail.com>
2023-01-08 23:28:33 +0100

    wallets sum

--- ancient-games-test/src/app/header/header.component.ts
+++ ancient-games-test/src/app/header/header.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {Service} from "../service";
 import {map, Observable} from "rxjs";
 import {User, Wallet} from "../types";
+
 
 @Component({
   selector: 'app-header',
@@ -10,9 +11,20 @@ import {User, Wallet} from "../types";
 })
 export class HeaderComponent {
   user$ = new Observable<User>;
-  wallets$ = new Observable<Wallet[]>;
+  walletsSum$ = new Observable<number>;
+  acc = 0;
+
   constructor(private service: Service) {
     this.user$ = this.service.user().pipe(map((user: User) => user));
-    this.wallets$ = this.service.user().pipe(map(user => user.wallets));
+    this.walletsSum$ = this.service
+      .user()
+      .pipe(
+        map(user => user),
+        map(user => user.wallets),
+        map(wallets => {
+          this.acc = wallets.reduce((acc: number, wallet: Wallet) => acc + wallet.amount!, 0)
+          return this.acc;
+        })
+      );
   }
 }
